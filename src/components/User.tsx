e805aa31fb00da299730669817f85a739d6ad915
Kunal Singh <kunalsin9h@gmail.com>
2024-05-24 23:10:44 +0530

    feat: using system-ui fonts

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -10,7 +10,7 @@ export default function User() {
           type="button"
           class="p-1 md:py-2 md:px-3 rounded border flex items-center text-sm md:gap-2 shadow active:bg-gray-100"
         >
-          <img src="/google.svg" alt="Google Icon" class="h-8 w-8" />
+          <img src="/google.svg" alt="Google Icon" class="h-6 w-6" />
           <span class="hidden md:block">Sign in with Google</span>
         </button>
       </a>
