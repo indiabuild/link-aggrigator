a2ed771054b54b2d5183d408b73dfc1668cbda7f
Kunal Singh <kunalsin9h@gmail.com>
2024-05-24 13:46:15 +0530

    feat: nav responsive

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,45 +10,89 @@ export default function Nav() {
       : "border-transparent hover:border-gray-300";
 
   return (
-    <nav class="border-b px-4 py-2">
+    <nav class="border-b px-2 py-2">
       <div class="flex items-center justify-between mx-auto container">
         {/* 
             Branding: Logo and Icon on the Left Side of Navigation
           */}
         <a href="/" class="flex items-center gap-2">
-          <img src="/foi_icon.svg" alt="Indian Flag" width={36} height={36} />
-          <p class="text-xl font-bold tracking-wide">IndiaBuild</p>
-          <span class="text-gray-400 text-xs text-blue-700 bg-blue-200 border rounded-xl border-blue-400 px-1 py-0 font-semibold">
+          <img
+            src="/foi_icon.svg"
+            alt="Indian Flag"
+            class="w-8 h-8 md:w-10 md:h-10"
+          />
+          <p class="text-md md:text-xl font-bold tracking-wide">IndiaBuild</p>
+          <span class="text-xs text-blue-700 bg-blue-200 border rounded-xl border-blue-400 px-1 py-0 font-semibold">
             beta
           </span>
         </a>
 
         {/* 
-            Navigations: Different pages
+            Navigations for big Screens: Different pages
+            
+            Hidden on small screen
           */}
-        <ul class="flex items-center">
-          <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
+        <ul class="flex items-center hidden md:flex gap-8">
+          <li class={`border-b-2 ${active("/")}`}>
             <a href="/">Home</a>
           </li>
 
-          <li class={`border-b-2 ${active("/new")} mx-1.5 sm:mx-6`}>
+          <li class={`border-b-2 ${active("/new")}`}>
             <a href="/new">New</a>
           </li>
 
-          <li class={`border-b-2 ${active("/past")} mx-1.5 sm:mx-6`}>
+          <li class={`border-b-2 ${active("/past")}`}>
             <a href="/past">Past</a>
           </li>
 
-          <li class={`border-b-2 ${active("/ask")} mx-1.5 sm:mx-6`}>
+          <li class={`border-b-2 ${active("/ask")}`}>
             <a href="/ask">Ask</a>
           </li>
-          <li class={`border-b-2 ${active("/comments")} mx-1.5 sm:mx-6`}>
+          {/* <li class={`border-b-2 ${active("/comments")}`}>
             <a href="/comments">Comments</a>
-          </li>
-          <li class={`border-b-2 ${active("/submit")} mx-1.5 sm:mx-6`}>
+          </li> */}
+          <li class={`border-b-2 ${active("/submit")}`}>
             <a href="/submit">Submit</a>
           </li>
         </ul>
+
+        {/* 
+            Navigations for small screens: Different pages
+            
+            Hidden on big screen
+          */}
+        <ul class="flex items-center fixed bottom-0 border-t justify-around w-full p-2 container md:hidden">
+          <li>
+            <a href="/">
+              <img src="/home.svg" class="w-6" />
+            </a>
+          </li>
+
+          <li>
+            <a href="/new">
+              <img src="/new.svg" class="w-6" />
+            </a>
+          </li>
+
+          <li>
+            <a href="/past">
+              <img src="/past.svg" class="w-6" />
+            </a>
+          </li>
+
+          <li>
+            <a href="/ask">
+              <img src="/ask.svg" class="w-6" />
+            </a>
+          </li>
+
+          <li>
+            <a href="/submit">
+              <img src="/submit.svg" class="w-6" />
+            </a>
+          </li>
+        </ul>
+
         {/* 
             User: Login, Logout and Profile
           */}
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,17 +1,11 @@
 export default function User() {
   return (
-    // <button
-    //   type="button"
-    //   class="rounded bg-teal-100 text-teal-800 font-bold py-1 px-4 hover:text-teal-400 hover:bg-teal-700 transition duration-100 delay-50"
-    // >
-    //   Login
-    // </button>
-
     <button
       type="button"
-      class="py-1 px-4 bg-black rounded-md text-white border hover:bg-black/80 font-semibold text-sm"
+      class="p-1 md:py-2 md:px-3 rounded border flex items-center text-sm md:gap-2 shadow"
     >
-      Login
+      <img src="/google.svg" alt="Google Icon" class="h-6 w-6" />
+      <span class="hidden md:block">Sign in with Google</span>
     </button>
   );
 }
