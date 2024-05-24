3344786b3deeec684bbee3962b2aedd222d0cf36
Kunal Singh <kunalsin9h@gmail.com>
2024-05-24 12:40:53 +0530

    feat(nav): added navigation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,56 @@
 import { useLocation } from "@solidjs/router";
+import User from "./User";
 
 export default function Nav() {
   const location = useLocation();
+
   const active = (path: string) =>
-    path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
+    path == location.pathname
+      ? "border-black"
+      : "border-transparent hover:border-gray-300";
+
   return (
-    <nav class="bg-sky-800">
-      <ul class="container flex items-center p-3 text-gray-200">
-        <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-          <a href="/">Home</a>
-        </li>
-        <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-          <a href="/about">About</a>
-        </li>
-      </ul>
+    <nav class="border-b px-4">
+      <div class="flex items-center justify-between mx-auto container">
+        {/* 
+            Branding: Logo and Icon on the Left Side of Navigation
+          */}
+        <a href="/" class="flex items-center gap-2">
+          <img src="/foi_icon.svg" alt="Indian Flag" width={36} height={36} />
+          <p class="text-xl font-bold tracking-wide">IndiaBuild</p>
+          <span class="text-gray-400 text-xs text-blue-500 bg-blue-200 border rounded-xl border-blue-400 px-1 py-0 font-semibold">
+            beta
+          </span>
+        </a>
+
+        {/* 
+            Navigations: Different pages
+          */}
+        <div>
+          <ul class="flex items-center p-3">
+            <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
+              <a href="/">Home</a>
+            </li>
+            <li class={`border-b-2 ${active("/new")} mx-1.5 sm:mx-6`}>
+              <a href="/about">New</a>
+            </li>
+            <li class={`border-b-2 ${active("/old")} mx-1.5 sm:mx-6`}>
+              <a href="/about">Old</a>
+            </li>
+            <li class={`border-b-2 ${active("/ask")} mx-1.5 sm:mx-6`}>
+              <a href="/about">Ask</a>
+            </li>
+            <li class={`border-b-2 ${active("/comments")} mx-1.5 sm:mx-6`}>
+              <a href="/about">Comments</a>
+            </li>
+          </ul>
+        </div>
+
+        {/* 
+            User: Login, Logout and Profile
+          */}
+        <User />
+      </div>
     </nav>
   );
 }
---
+++ src/components/User.tsx
@@ -0,0 +1,17 @@
+export default function User() {
+  return (
+    // <button
+    //   type="button"
+    //   class="rounded bg-teal-100 text-teal-800 font-bold py-1 px-4 hover:text-teal-400 hover:bg-teal-700 transition duration-100 delay-50"
+    // >
+    //   Login
+    // </button>
+
+    <button
+      type="button"
+      class="py-1 px-4 bg-black rounded-md text-white border hover:bg-black/80"
+    >
+      Login
+    </button>
+  );
+}
