5b423febf0580684f708ea2d376b7563cb9cef1d
Kunal Singh <kunalsin9h@gmail.com>
2024-05-25 21:29:10 +0530

    minor

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,10 +5,12 @@ export default function Home() {
         <Options
           name="Top Categories"
           items={["Manufacturing", "AI/ML", "Internet"]}
+          slug="categories"
         />
         <Options
           name="Top Tags"
           items={["semicundurcot", "python", "linear-regression"]}
+          slug="tags"
         />
       </div>
       <div class="basis-2/3">
@@ -18,13 +20,37 @@ export default function Home() {
   );
 }
 
-function Options({ name, items }: { name: string; items: string[] }) {
+function Options({
+  name,
+  slug,
+  items,
+}: {
+  name: string;
+  slug: string;
+  items: string[];
+}) {
   return (
     <div class="flex flex-col gap-2">
       <p class="font-bold text-xl">{name}</p>
-      <ul class="text-sm flex gap-2 flex-wrap">
+      <ul class="text-sm flex gap-2 flex-wrap ">
         {items.map((i) => (
-          <li>{i}</li>
+          <li
+            class="bg-gray-200 px-2 rounded-xl text-sm cursor-pointer  hover:bg-gray-300 active:bg-gray-400"
+            onClick={() => {
+              // Get the current URL parameters
+              let urlParams = new URLSearchParams(window.location.search);
+              // Add a new query string parameter
+              urlParams.append(slug, i);
+              // Generate the updated URL with the new parameter
+              let updatedUrl = `${window.location.origin}${
+                window.location.pathname
+              }?${urlParams.toString()}`;
+              // Load the window with the updated URL
+              window.location.href = updatedUrl;
+            }}
+          >
+            {i}
+          </li>
         ))}
       </ul>
     </div>
---
+++ src/routes/new/index.tsx
@@ -0,0 +1,3 @@
+export default function New() {
+  return <p>New Post</p>;
+}
