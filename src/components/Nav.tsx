add2d9f00ec2472b97fdd5608e1471d44157f23f
Kunal Singh <kunalsin9h@gmail.com>
2024-05-25 17:31:24 +0530

    minor

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -14,94 +14,95 @@ export default function Nav() {
     path == location.pathname && "bg-cyan-200 rounded-full";
 
   return (
-    <nav class="border-b px-2 py-2">
-      <div class="flex items-center justify-between mx-auto container">
-        {/* 
+    <nav>
+      <div class="border-b px-2 py-3">
+        <div class="relative flex items-center justify-between mx-auto container">
+          {/* 
             Branding: Logo and Icon on the Left Side of Navigation
           */}
-        <A href="/" class="flex items-center gap-2">
-          <img
-            src="/foi_icon.svg"
-            alt="Indian Flag"
-            class="w-8 h-8 md:w-10 md:h-10"
-          />
-          <p class="text-md md:text-xl font-bold tracking-wide">IndiaBuild</p>
-          <span class="text-xs text-purple-700 bg-purple-200 border rounded-xl border-purple-400 px-1 py-0 font-semibold">
-            dev release
-          </span>
-        </A>
+          <A href="/" class="flex items-center gap-2">
+            <img
+              src="/foi_icon.svg"
+              alt="Indian Flag"
+              class="w-8 h-8 md:w-10 md:h-10"
+            />
+            <p class="text-lg md:text-xl font-bold tracking-wide">IndiaBuild</p>
+            <span class="text-xs text-purple-700 bg-purple-200 border rounded-xl border-purple-400 px-1 py-0 font-semibold">
+              dev release
+            </span>
+          </A>
 
-        {/* 
+          {/* 
             Navigations for big Screens: Different pages
             
             Hidden on small screen
           */}
-        <ul class="flex items-center hidden md:flex gap-8">
-          <li class={`border-b-2 ${active("/")}`}>
-            <A href="/">Home</A>
-          </li>
+          <ul class="flex items-center hidden md:flex gap-8 absolute left-[50%] transform -translate-x-1/2">
+            <li class={`border-b-2 ${active("/")}`}>
+              <A href="/">Home</A>
+            </li>
 
-          <li class={`border-b-2 ${active("/new")}`}>
-            <A href="/new">New</A>
-          </li>
+            <li class={`border-b-2 ${active("/new")}`}>
+              <A href="/new">New</A>
+            </li>
 
-          <li class={`border-b-2 ${active("/past")}`}>
-            <A href="/past">Past</A>
-          </li>
+            <li class={`border-b-2 ${active("/past")}`}>
+              <A href="/past">Past</A>
+            </li>
 
-          <li class={`border-b-2 ${active("/Ask")}`}>
-            <A href="/Ask">Ask</A>
-          </li>
-          {/* <li class={`border-b-2 ${Active("/comments")}`}>
+            <li class={`border-b-2 ${active("/Ask")}`}>
+              <A href="/Ask">Ask</A>
+            </li>
+            {/* <li class={`border-b-2 ${Active("/comments")}`}>
             <A href="/comments">Comments</A>
           </li> */}
-          <li class={`border-b-2 ${active("/submit")}`}>
-            <A href="/submit">Submit</A>
-          </li>
-        </ul>
+            <li class={`border-b-2 ${active("/submit")}`}>
+              <A href="/submit">Submit</A>
+            </li>
+          </ul>
 
-        {/* 
+          {/* 
+            User: Login, Logout and Profile
+          */}
+          <User />
+        </div>
+      </div>
+      {/* 
             Navigations for small screens: Different pages
             
             Hidden on big screen
           */}
-        <ul class="flex items-center fixed bottom-0 border-t justify-around w-full p-2 container md:hidden">
-          <li>
-            <A href="/">
-              <img src="/home.svg" class={`w-6 ${activeIcon("/")}`} />
-            </A>
-          </li>
+      <ul class="flex px-2 py-3 items-center fixed bottom-0 border-t justify-around w-full container md:hidden">
+        <li>
+          <A href="/">
+            <img src="/home.svg" class={`w-6 ${activeIcon("/")}`} />
+          </A>
+        </li>
 
-          <li>
-            <A href="/new">
-              <img src="/new.svg" class={`w-6 ${activeIcon("/new")}`} />
-            </A>
-          </li>
+        <li>
+          <A href="/new">
+            <img src="/new.svg" class={`w-6 ${activeIcon("/new")}`} />
+          </A>
+        </li>
 
-          <li>
-            <A href="/past">
-              <img src="/past.svg" class={`w-6 ${activeIcon("/past")}`} />
-            </A>
-          </li>
+        <li>
+          <A href="/past">
+            <img src="/past.svg" class={`w-6 ${activeIcon("/past")}`} />
+          </A>
+        </li>
 
-          <li>
-            <A href="/ask">
-              <img src="/ask.svg" class={`w-6 ${activeIcon("/ask")}`} />
-            </A>
-          </li>
+        <li>
+          <A href="/ask">
+            <img src="/ask.svg" class={`w-6 ${activeIcon("/ask")}`} />
+          </A>
+        </li>
 
-          <li>
-            <A href="/submit">
-              <img src="/submit.svg" class={`w-6 ${activeIcon("/submit")}`} />
-            </A>
-          </li>
-        </ul>
-
-        {/* 
-            User: Login, Logout and Profile
-          */}
-        <User />
-      </div>
+        <li>
+          <A href="/submit">
+            <img src="/submit.svg" class={`w-6 ${activeIcon("/submit")}`} />
+          </A>
+        </li>
+      </ul>
     </nav>
   );
 }
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -13,23 +13,21 @@ export default function User() {
   });
 
   return (
-    <div>
-      <Show
-        when={user()}
-        fallback={
-          <a href="/api/auth/google">
-            <button
-              type="button"
-              class="rounded border p-1 px-2 flex items-center text-sm md:gap-2 shadow active:bg-gray-100"
-            >
-              <img src="/google.svg" alt="Google Icon" class="h-5 w-5" />
-              <span class="hidden md:block">Sign in with Google</span>
-            </button>
-          </a>
-        }
-      >
-        <UserProfile user={user() as UserType} />
-      </Show>
-    </div>
+    <Show
+      when={user()}
+      fallback={
+        <a href="/api/auth/google">
+          <button
+            type="button"
+            class="rounded border p-1 flex items-center text-sm md:gap-2 shadow active:bg-gray-100"
+          >
+            <img src="/google.svg" alt="Google Icon" class="h-5 w-5" />
+            <span class="hidden md:block">Sign in with Google</span>
+          </button>
+        </a>
+      }
+    >
+      <UserProfile user={user() as UserType} />
+    </Show>
   );
 }
