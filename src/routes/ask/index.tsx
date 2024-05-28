dbd2791e5069d6996b247fc50fb9772e0efa3a29
Kunal Singh <kunalsin9h@gmail.com>
2024-05-28 22:21:12 +0530

    feat: ather routes

---
+++ src/routes/ask/index.tsx
@@ -0,0 +1,11 @@
+import FilterLists from "~/components/FilterLists";
+
+export default function AskQuestions() {
+  return (
+    <FilterLists>
+      <div class="text-lg font-bold text-center">
+        <p>Comming Soon...</p>
+      </div>
+    </FilterLists>
+  );
+}
---
+++ src/routes/past/index.tsx
@@ -0,0 +1,11 @@
+import FilterLists from "~/components/FilterLists";
+
+export default function PastLinks() {
+  return (
+    <FilterLists>
+      <div class="text-lg font-bold text-center">
+        <p>Comming Soon...</p>
+      </div>
+    </FilterLists>
+  );
+}
